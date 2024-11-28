3a39588bdda726efe5753c561839212c248e3ae3
César Fernando <tanquistagamer@hotmail.com>
2024-11-28 02:23:46 -0600

    Tarea9

---
+++ tarea9/resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content_header')
+    <h1>Soporte</h1>
+@stop
+
+@section('content')
+    <p>Contactanos por este medio.</p>
+@stop
---
+++ tarea9/resources/views/pages/photos.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content_header')
+    <h1>Photos</h1>
+@stop
+
+@section('content')
+    <p>Bienvenidos a la Pagina de Multimedia.</p>
+@stop
